146cdd69c745fccf95d55343ee451f3bfaf14297
Grzegorz Karol Karch <karchgz@nin.visus.uni-stuttgart.de>
2015-03-03 15:58:27 +0100

    added label

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,13 +1,24 @@
 #include "mainwindow.h"
 #include "glwidget.h"
+#include <QVBoxLayout>
 #include <QHBoxLayout>
-
+#include <QLabel>
+#include <QWidget>
 
 MainWindow::MainWindow()
 {
     glwidget = new GLWidget;
-    QHBoxLayout *mainLayout = new QHBoxLayout;
+    QVBoxLayout *mainLayout = new QVBoxLayout;
     mainLayout->addWidget(glwidget);
+
+    QHBoxLayout *lightEditorLayout = new QHBoxLayout;
+
+    QLabel *lightColorLabel = new QLabel(this);
+    lightColorLabel->setText("Light color");
+    lightEditorLayout->addWidget(lightColorLabel);
+
+
+    mainLayout->addLayout(lightEditorLayout);
     setCentralWidget(new QWidget);
     centralWidget()->setLayout(mainLayout);
 }
